285ffd0fb384704e2b0569b165edbc672df289aa
Luifer1994 <almendralesluifer@gmail.com>
2025-01-31 19:53:36 -0500

    validate date fetch

--- src/offline/fetchAllCommonData.ts
+++ src/offline/fetchAllCommonData.ts
@@ -1,10 +1,10 @@
 // src/offline/fetchAllCommonData.ts
 
-import { useRolesAndPermissions } from "@/modules/rolesAndPermissions/composables/useRolesAndPermissions"
-import { useDocumentType } from "@/modules/documentTypes/composables/useDocumentType"
-import { useCity } from "@/modules/cities/composables/useCity"
-import { useLoans } from "@/modules/loans/composables/useLoans"
-import { useClients } from "@/modules/clients/composables/useClients"
+import { useRolesAndPermissions } from "@/modules/rolesAndPermissions/composables/useRolesAndPermissions";
+import { useDocumentType } from "@/modules/documentTypes/composables/useDocumentType";
+import { useCity } from "@/modules/cities/composables/useCity";
+import { useLoans } from "@/modules/loans/composables/useLoans";
+import { useClients } from "@/modules/clients/composables/useClients";
 // ... y así con tus otros catálogos
 
 /**
@@ -13,21 +13,23 @@ import { useClients } from "@/modules/clients/composables/useClients"
  */
 export async function fetchAllCommonData() {
   try {
-    const { fetchRoles } = useRolesAndPermissions()
-    const { fetchDocumentTypes } = useDocumentType()
-    const { fetchAllCities } = useCity()
-    const { fetchLoans } = useLoans()
-    const { fetchClientsActive } = useClients()
+    const { fetchRoles } = useRolesAndPermissions();
+    const { fetchDocumentTypes } = useDocumentType();
+    const { fetchAllCities } = useCity();
+
     // Llamas a cada uno
-    await Promise.all([
-      fetchRoles(),
-      fetchDocumentTypes(),
-      fetchAllCities(),
-      fetchLoans(),
-      fetchClientsActive(),
-      // etc.
-    ])
+    await Promise.all([fetchRoles(), fetchDocumentTypes(), fetchAllCities()]);
+
+    /*  estos solo si en el local store existe tenant y tiene un id y si el validTenant es true tambien en localStore */
+    if (
+      localStorage.getItem("tenant") &&
+      localStorage.getItem("validTenant") === "true"
+    ) {
+      const { fetchLoans } = useLoans();
+      const { fetchClientsActive } = useClients();
+      await Promise.all([fetchLoans(), fetchClientsActive()]);
+    }
   } catch (error) {
-    console.error("Error al cargar los catálogos comunes:", error)
+    console.error("Error al cargar los catálogos comunes:", error);
   }
 }
